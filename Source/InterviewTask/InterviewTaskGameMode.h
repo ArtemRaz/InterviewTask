1d26803356ae4265f0680173f022f3133656564d
ArtemRaz <artemraz@mail.ru>
2024-03-06 13:17:48 +0300

    init

---
+++ Source/InterviewTask/InterviewTaskGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InterviewTaskGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AInterviewTaskGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AInterviewTaskGameMode();
+};
+
+
+
